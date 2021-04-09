5673789710a4a491c91e5f0060b66353ecb09616
Shahid Saddique <shahidali6@gmail.com>
2021-04-09 18:05:25 +0500

    Working group of Read users from group added.

---
+++ ReadUsersOfGroups/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4ReadGroups
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // ArrayList myGroups = GetGroupMembers("Administrators");
+            ArrayList myGroups = GetGroupMembers("Users");
+            foreach (string item in myGroups)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+        public static ArrayList GetGroupMembers(string sGroupName)
+        {
+            ArrayList myItems = new ArrayList();
+            GroupPrincipal oGroupPrincipal = GetGroup(sGroupName);
+
+            PrincipalSearchResult<Principal> oPrincipalSearchResult = oGroupPrincipal.GetMembers();
+
+            foreach (Principal oResult in oPrincipalSearchResult)
+            {
+                myItems.Add(oResult.Name);
+            }
+            return myItems;
+        }
+
+        public static GroupPrincipal GetGroup(string sGroupName)
+        {
+            PrincipalContext oPrincipalContext = GetPrincipalContext();
+
+            GroupPrincipal oGroupPrincipal = GroupPrincipal.FindByIdentity(oPrincipalContext, sGroupName);
+            return oGroupPrincipal;
+        }
+
+        public static PrincipalContext GetPrincipalContext()
+        {
+            PrincipalContext oPrincipalContext = new PrincipalContext(ContextType.Machine);
+            return oPrincipalContext;
+        }
+
+    }
+}
