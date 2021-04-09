993cc8078fdbef2c679b655a8aba3a6ad2848dd6
Shahid Saddique <shahidali6@gmail.com>
2021-04-09 18:01:51 +0500

    PDF merge code added.

---
+++ MergePDFFilesv1a/ConsoleApp4MergePDFFiles/Program.cs
@@ -0,0 +1,68 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4MergePDFFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MergePDF(@"J:\Dropbox\09-FreelancerWork\Fiverr\Orders\orange109\20210315\Splitted\לחם ושות' לחם ושעושועים בעמ בן יהודה  - 2555 - 02.2021.pdf", @"J:\Dropbox\09-FreelancerWork\Fiverr\Orders\orange109\20210315\Splitted\יש לי בוטן שיווק מזון בעמ - 253 - 02.2021.pdf");
+        }
+        private static void MergePDF(string File1, string File2)
+        {
+            string[] fileArray = new string[3];
+            fileArray[0] = File1;
+            fileArray[1] = File2;
+
+            PdfReader reader = null;
+            Document sourceDocument = null;
+            PdfCopy pdfCopyProvider = null;
+            PdfImportedPage importedPage;
+            string outputPdfPath = @"d:/newFile.pdf";
+
+            sourceDocument = new Document();
+            pdfCopyProvider = new PdfCopy(sourceDocument, new System.IO.FileStream(outputPdfPath, System.IO.FileMode.Create));
+
+            //output file Open  
+            sourceDocument.Open();
+
+
+            //files list wise Loop  
+            for (int f = 0; f < fileArray.Length - 1; f++)
+            {
+                int pages = TotalPageCount(fileArray[f]);
+
+                reader = new PdfReader(fileArray[f]);
+                //Add pages in new file  
+                for (int i = 1; i <= pages; i++)
+                {
+                    importedPage = pdfCopyProvider.GetImportedPage(reader, i);
+                    pdfCopyProvider.AddPage(importedPage);
+                }
+
+                reader.Close();
+            }
+            //save the output file  
+            sourceDocument.Close();
+        }
+
+        private static int TotalPageCount(string file)
+        {
+            using (StreamReader sr = new StreamReader(System.IO.File.OpenRead(file)))
+            {
+                Regex regex = new Regex(@"/Type\s*/Page[^s]");
+                MatchCollection matches = regex.Matches(sr.ReadToEnd());
+
+                return matches.Count;
+            }
+        }
+    }
+}
