b27ca6579218026b5e313eb628667e11381abe4b
Shahid Saddique <shahidali6@gmail.com>
2021-04-12 20:07:02 +0500

    Updated code of user group added.

--- ReadUsersOfGroups/Program.cs
+++ ReadUsersOfGroups/Program.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,15 +12,45 @@ namespace ConsoleApp4ReadGroups
 {
     class Program
     {
+        static List<string> listOfSystemGroups = new List<string>();
+        static List<string> listOfUsers = new List<string>();
+        List<GroupStructure> listOfGroupsUsers = new List<GroupStructure>();
+
         static void Main(string[] args)
         {
+            string roamingFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData, Environment.SpecialFolderOption.None);
+            string appPath = Path.Combine(roamingFolderPath, "VisualSVNHook\\");
+
+            listOfSystemGroups = ExtractNameOfGroups();
+
+            listOfGroupsUsers =  ExtractUsersOfGroups(listOfSystemGroups);
+        }
+
+        private static List<GroupStructure> ExtractUsersOfGroups(List<GroupStructure> listOfGroupNames)
+        {
+            // ArrayList myGroups = GetGroupMembers("Administrators");
+            foreach (var group in listOfGroupNames)
+            {
+                Console.WriteLine("Group Name: "+group);
+                ArrayList myGroups = GetGroupMembers(group);
+                foreach (string item in myGroups)
+                {
+                    Console.WriteLine("Group Member: " + item);
+                } 
+            }
+            Console.ReadLine();
+        }
+
+        private static List<string> ExtractNameOfGroups()
+        {
+            List<string> listOfGroups = new List<string>();
             DirectoryEntry machine = new DirectoryEntry("WinNT://" + Environment.MachineName + ",Computer");
             foreach (DirectoryEntry child in machine.Children)
             {
-                
                 if (child.SchemaClassName == "Group")
                 {
-                    Console.WriteLine("Group: " +child.Name);
+                    listOfGroups.Add(child.Name);
+                    Console.WriteLine("Group: " + child.Name);
                     foreach (var childchild in child.Children)
                     {
                         Console.WriteLine("Group: childchild" + childchild);
@@ -28,18 +59,19 @@ namespace ConsoleApp4ReadGroups
                 }
                 if (child.SchemaClassName == "User")
                 {
+                    listOfUsers.Add(child.Name);
                     Console.WriteLine("User: " + child.Name);
                     continue;
                 }
                 //Console.WriteLine(child.Name);
             }
-            // ArrayList myGroups = GetGroupMembers("Administrators");
-            ArrayList myGroups = GetGroupMembers("lalala");
-            foreach (string item in myGroups)
-            {
-                Console.WriteLine("Group Member: "+item);
-            }
-            Console.ReadLine();
+            return listOfGroups;
+        }
+
+        struct GroupStructure
+        {
+            public string groupName;
+            public List<string> listOfUsers;
         }
         public static ArrayList GetGroupMembers(string sGroupName)
         {
@@ -50,7 +82,7 @@ namespace ConsoleApp4ReadGroups
 
             foreach (Principal oResult in oPrincipalSearchResult)
             {
-                myItems.Add(oResult.Name);
+                myItems.Add(oResult.SamAccountName);
             }
             return myItems;
         }
