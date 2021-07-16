b92c3f870d1cdd5ccc4c3385029e94d0c76c305f
Shahid Saddique <msaddique@Powersoft19.com>
2021-07-16 16:00:10 +0500

    Must email address added on every hook on every project.

--- WriteSVNHookfromGroupsAndUsers/Hook.cs
+++ WriteSVNHookfromGroupsAndUsers/Hook.cs
@@ -12,47 +12,68 @@ namespace ConsoleApp4ReadGroups
         public void WriteHookFile(List<Company> listToReturn, string xMLFileName, string postHookFilename)
         {
             string lineTerminaltor = " ^";
-            string visualSVNServerHookPath = @"%VISUALSVN_SERVER%\bin\VisualSVNServerHooks.exe";
-            string commitNotification = "commit-notification \"% 1\" -r %2";
-            string filterKeyword = " --filter ";
-            string filterPath = "/dsfdsf";
+            string visualSVNServerHookPath = "\"%VISUALSVN_SERVER%\\bin\\VisualSVNServerHooks.exe\"";
+            string commitNotification = "    commit-notification \"%1\" -r %2";
+            string filterKeyword = "    --filter ";
             string smtpKeyword = " --smtp-server ";
             string smtpServerAddress = "mail2.powersoft19.com";
-            string fromKeyword = " --from ";
+            string fromKeyword = "    --from ";
             string toKeyword = " --to ";
             string noDifferenceKeyword = " --no-diffs ";
             string fromEmail = "mossadmin240@powersoft19.com";
-            string toEmail = "msaddique@powersoft19.com";
             string semiColon = ";";
+            string powerDomain = "POWER\\";
+            string powersoft19Dotcom = "@powersoft19.com";
+            string[] mustEmailAddress = { "asadiq@powersoft19.com", "msaddique@powersoft19.com" };
 
 
-            using (StreamWriter sw = new StreamWriter("hookFile.txt"))
+            string hookFile = "hookFile.txt";
+            string commitFile = "post-commit.cmd";
+
+            if (File.Exists(hookFile))
             {
+                File.Delete(hookFile);
+            }
                 foreach (var company in listToReturn)
+                {
+                string cleanCompanyName = company.company_name.Replace("\"", string.Empty);
+                string path = "Q:\\temp\\"+cleanCompanyName+"\\hooks\\" ;
+
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                using (StreamWriter sw = new StreamWriter(path+commitFile, false))
                 {
                     foreach (var project in company.projects)
                     {
                         StringBuilder emailList = new StringBuilder();
                         foreach (var users in project.groups)
                         {
+                            if (users.group_name.ToLower().Contains(powerDomain.ToLower()))
+                            {
+                                users.group_name = users.group_name.Replace(powerDomain, string.Empty);
+                                users.group_name = users.group_name.Replace("\"", string.Empty);
+                                users.group_name = users.group_name + powersoft19Dotcom;
+                            }
                             emailList.Append(users.group_name + semiColon);
                         }
+
+                        foreach (var email in mustEmailAddress)
+                        {
+                            if (!emailList.ToString().Contains(email))
+                            {
+                                emailList.Append(email + semiColon);
+                            }
+                        }
+
                         sw.WriteLine(visualSVNServerHookPath + lineTerminaltor);
                         sw.WriteLine(commitNotification + lineTerminaltor);
                         sw.WriteLine(fromKeyword + fromEmail + toKeyword + emailList + lineTerminaltor);
-                        sw.WriteLine(filterKeyword + "\"" + project.project_name + "\"" + smtpKeyword + smtpServerAddress + noDifferenceKeyword + Environment.NewLine);
+                        sw.WriteLine(filterKeyword + project.project_name + smtpKeyword + smtpServerAddress + noDifferenceKeyword + Environment.NewLine);
                     }
                 }
             }
-            //foreach (var company in listToReturn)
-            //    {
-
-
-            //        sw.WriteLine(visualSVNServerHookPath + lineTerminaltor);
-            //        sw.WriteLine(commitNotification + lineTerminaltor);
-            //        sw.WriteLine(fromKeyword + fromEmail + toKeyword + toEmail + lineTerminaltor);
-            //        sw.WriteLine(filterKeyword+filterPath+smtpKeyword+smtpServerAddress+noDifferenceKeyword+Environment.NewLine);
-            //    }
         }
     }
 }
