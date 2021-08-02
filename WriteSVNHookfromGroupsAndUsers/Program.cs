1f3243e4d2dcc09dda2d3aff735e0e37a9e86b5b
Shahid Saddique <msaddique@Powersoft19.com>
2021-08-02 15:12:30 +0500

    Remove most of extra code. Other extra code will remove when have time.

--- WriteSVNHookfromGroupsAndUsers/Program.cs
+++ WriteSVNHookfromGroupsAndUsers/Program.cs
@@ -6,6 +6,7 @@ using System.DirectoryServices.AccountManagement;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 
@@ -20,41 +21,18 @@ namespace ConsoleApp4ReadGroups
             string postHookFilename = "post-commit.cmd";
             string csvFilename = "AccessReport.csv";
 
-            List<string> listOfSystemGroups = new List<string>();
-            List<GroupStructure> listOfGroupUsers = new List<GroupStructure>();
-            List<VisualSVNHookStructure> listOfHookStructure = new List<VisualSVNHookStructure>();
-
             string roamingFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData, Environment.SpecialFolderOption.None);
             string appPath = Path.Combine(roamingFolderPath, "VisualSVNHook\\");
 
-            GroupUser groupUser = new GroupUser();
             Hook hook = new Hook();
-            CSVFileReader cSVFileReader = new CSVFileReader();
-            CSVToJSON cSVToJSON = new CSVToJSON();
-            JSONwriterClass jSONwriterClass = new JSONwriterClass();
             ReferenceTaswar referenceTaswar = new ReferenceTaswar();
 
-            //listOfSystemGroups = groupUser.ExtractNameOfGroups();
+            var structureResult =  referenceTaswar.Function(@"F:\AccessReport.csv");
 
-            //listOfGroupUsers = groupUser.ExtractUsersOfGroups(listOfSystemGroups);
+            bool status = hook.WriteHookFile(structureResult, appPath + xMLFileName, appPath + postHookFilename);
 
-            XMLFile xMLFile = new XMLFile();
-
-            xMLFile.WriteXMLFile(listOfGroupUsers, appPath + xMLFileName );
-
-            List<CSVFileStructure> listToReturn = new List<CSVFileStructure>();
-
-            //listToReturn = cSVFileReader.ReadCSVFile(appPath + csvFilename);
-            //listToReturn = cSVFileReader.ReadCSVFileNew(appPath + csvFilename);
-            //listToReturn = cSVFileReader.ReadCSVFileNew1(appPath + csvFilename);
-
-            var structureResult =  referenceTaswar.Function(@"Q:\temp\AccessReport.csv");
-
-            //hook.WriteHookFile(listToReturn, appPath + xMLFileName, appPath + postHookFilename);
-            hook.WriteHookFile(structureResult, appPath + xMLFileName, appPath + postHookFilename);
-
-            //cSVToJSON.CSVToJSONFunc(appPath + csvFilename);
-            //jSONwriterClass.JSONWriteFunc(appPath + csvFilename);
+            Console.WriteLine("Completed Hook Generation!");
+            Thread.Sleep(1000);
         }
     }
 }
