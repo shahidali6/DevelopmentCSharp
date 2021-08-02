f9b8d77692427222319f80cee0ea3fa002857859
Shahid Saddique <msaddique@Powersoft19.com>
2021-08-02 15:08:00 +0500

    Restructured and Changed.

--- WriteSVNHookfromGroupsAndUsers/Hook.cs
+++ WriteSVNHookfromGroupsAndUsers/Hook.cs
@@ -9,71 +9,111 @@ namespace ConsoleApp4ReadGroups
 {
     class Hook
     {
-        public void WriteHookFile(List<Company> listToReturn, string xMLFileName, string postHookFilename)
+        public bool WriteHookFile(List<Company> listToReturn, string xMLFileName, string postHookFilename)
         {
-            string lineTerminaltor = " ^";
-            string visualSVNServerHookPath = "\"%VISUALSVN_SERVER%\\bin\\VisualSVNServerHooks.exe\"";
-            string commitNotification = "    commit-notification \"%1\" -r %2";
-            string filterKeyword = "    --filter ";
-            string smtpKeyword = " --smtp-server ";
-            string smtpServerAddress = "mail2.powersoft19.com";
-            string fromKeyword = "    --from ";
-            string toKeyword = " --to ";
-            string noDifferenceKeyword = " --no-diffs ";
-            string fromEmail = "mossadmin240@powersoft19.com";
-            string semiColon = ";";
-            string powerDomain = "POWER\\";
-            string powersoft19Dotcom = "@powersoft19.com";
-            string[] mustEmailAddress = { "asadiq@powersoft19.com", "msaddique@powersoft19.com" };
-
-
-            string hookFile = "hookFile.txt";
-            string commitFile = "post-commit.cmd";
-
-            if (File.Exists(hookFile))
+            try
             {
-                File.Delete(hookFile);
-            }
+                if (listToReturn == null)
+                {
+                    return false;
+                }
+
+                string lineTerminaltor = " ^";
+                string visualSVNServerHookPath = "\"%VISUALSVN_SERVER%\\bin\\VisualSVNServerHooks.exe\"";
+                string commitNotification = "    commit-notification \"%1\" -r %2";
+                string filterKeyword = "    --filter ";
+                string smtpKeyword = " --smtp-server ";
+                string smtpServerAddress = "mail.powersoft19.com";
+                string fromKeyword = "    --from ";
+                string toKeyword = " --to ";
+                string noDifferenceKeyword = " --no-diffs ";
+                string fromEmail = "mossadmin240@powersoft19.com";
+                string semiColon = ";";
+                string powerDomain = "POWER\\";
+                string powersoft19Dotcom = "@powersoft19.com";
+                string[] mustEmailAddress = { "asadiq@powersoft19.com", "msaddique@powersoft19.com" };
+
+
+                string hookFile = "hookFile.txt";
+                string commitFile = "post-commit.cmd";
+
+                if (File.Exists(hookFile))
+                {
+                    File.Delete(hookFile);
+                }
+
+                List<char> listOfAllInvalidChars = new List<char>();
+
+                // Get a list of invalid path characters.
+                // Get a list of invalid file characters.
+                //combine both arrays to list
+
+                listOfAllInvalidChars = Path.GetInvalidPathChars().Concat(Path.GetInvalidFileNameChars()).Distinct().ToList();
+
+
                 foreach (var company in listToReturn)
                 {
-                string cleanCompanyName = company.company_name.Replace("\"", string.Empty);
-                string path = "Q:\\temp\\"+cleanCompanyName+"\\hooks\\" ;
+                    company.company_name = company.company_name.Replace("\"", "");
+                    if (CheckInvalidcharacter(listOfAllInvalidChars, company.company_name))
+                        continue;
 
-                if (!Directory.Exists(path))
-                {
-                    Directory.CreateDirectory(path);
-                }
-                using (StreamWriter sw = new StreamWriter(path+commitFile, false))
-                {
-                    foreach (var project in company.projects)
+                    string cleanCompanyName = company.company_name.Replace("\"", string.Empty);
+                    string path = "F:\\Repositories\\" + cleanCompanyName + "\\hooks\\";
+
+                    if (!Directory.Exists(path))
                     {
-                        StringBuilder emailList = new StringBuilder();
-                        foreach (var users in project.groups)
+                        Directory.CreateDirectory(path);
+                    }
+                    using (StreamWriter sw = new StreamWriter(path + commitFile, false))
+                    {
+                        foreach (var project in company.projects)
                         {
-                            if (users.group_name.ToLower().Contains(powerDomain.ToLower()))
+                            StringBuilder emailList = new StringBuilder();
+                            foreach (var users in project.groups)
                             {
-                                users.group_name = users.group_name.Replace(powerDomain, string.Empty);
-                                users.group_name = users.group_name.Replace("\"", string.Empty);
-                                users.group_name = users.group_name + powersoft19Dotcom;
+                                if (users.group_name.ToLower().Contains(powerDomain.ToLower()))
+                                {
+                                    users.group_name = users.group_name.Replace(powerDomain, string.Empty);
+                                    users.group_name = users.group_name.Replace("\"", string.Empty);
+                                    users.group_name = users.group_name + powersoft19Dotcom;
+                                }
+                                emailList.Append(users.group_name + semiColon);
                             }
-                            emailList.Append(users.group_name + semiColon);
-                        }
 
-                        foreach (var email in mustEmailAddress)
-                        {
-                            if (!emailList.ToString().Contains(email))
+                            foreach (var email in mustEmailAddress)
                             {
-                                emailList.Append(email + semiColon);
+                                if (!emailList.ToString().Contains(email))
+                                {
+                                    emailList.Append(email + semiColon);
+                                }
                             }
-                        }
 
-                        sw.WriteLine(visualSVNServerHookPath + lineTerminaltor);
-                        sw.WriteLine(commitNotification + lineTerminaltor);
-                        sw.WriteLine(fromKeyword + fromEmail + toKeyword + emailList + lineTerminaltor);
-                        sw.WriteLine(filterKeyword + project.project_name + smtpKeyword + smtpServerAddress + noDifferenceKeyword + Environment.NewLine);
+                            sw.WriteLine(visualSVNServerHookPath + lineTerminaltor);
+                            sw.WriteLine(commitNotification + lineTerminaltor);
+                            sw.WriteLine(fromKeyword + fromEmail + toKeyword + emailList + lineTerminaltor);
+                            sw.WriteLine(filterKeyword + project.project_name + smtpKeyword + smtpServerAddress + noDifferenceKeyword + Environment.NewLine);
+                        }
                     }
                 }
+                return true;
             }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private static bool CheckInvalidcharacter(List<char> listOfAllInvalidChars, string companyName)
+        {
+            foreach (var character in listOfAllInvalidChars)
+            {
+                if (companyName.Contains(character))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
