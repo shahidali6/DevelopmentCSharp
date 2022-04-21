5a54ca916875ecb1b9e3ef29e7d34554ee77ada6
Shahid Saddique <msaddique@Powersoft19.com>
2022-04-21 19:56:47 +0500

    Application settings added.

---
+++ WriteSVNHookfromGroupsAndUsers/ApplicationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SVNHookGenerator
+{
+    internal class ApplicationSettings
+    {
+        public string Settingsfile { get; set; }
+        public string RepositoriesPath { get; set; }
+        public string PostHookfileName { get; set; }
+        public string AccessCSVFileName { get; set; }
+        public string AplicationDirectory { get; set; }
+    }
+}
